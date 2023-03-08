e3d4f39f5524a6321299caa51bedd227475c56d1
benebsiny <stu995106@hotmail.com.tw>
2023-03-08 11:14:27 -0500

    [WIP] Pywikibot-like bots
    1. User can read a page
    2. Support bot login
    3. Bot can edit/delete a page with login status

---
+++ api/bot.ts
@@ -0,0 +1,102 @@
+import * as requests from "./requests.ts";
+import {Client} from "./client.ts";
+
+export default class Bot {
+    static site(url: string): Site {
+        return new Site(url);
+    }
+
+    static async page(site: Site, title: string): Promise<Page> {
+        const res = await site.client.invoke(
+            requests.query({
+                titles: title,
+                prop: "revisions",
+                rvprop: "content",
+                rvslots: "main"
+            })
+        );
+        const revisionId = Object.keys(res.query.pages)[0];
+        const text = res.query.pages[revisionId]["revisions"][0]["slots"]["main"]["*"];
+        return new Page(
+            site, title, text, Number(revisionId)
+        );
+    }
+}
+
+export class Site {
+    public readonly url: string = "";
+    client: Client;
+    csrftoken = "";
+
+    constructor(url: string, code?: string) {
+        this.url = url;
+        this.client = new Client(this.url);
+    }
+
+    async login(botAccount: string, botPassword: string) {
+
+        // Login step reference https://www.mediawiki.org/wiki/API:Edit#Example
+        // Step 1. Get login token
+        const {query: {tokens: {logintoken: lgtoken}}} = await this.client.invoke(
+            requests.query({meta: "tokens", type: "login"}),
+        );
+
+        // Step 2. Login with bot account, bot password, and login token
+        await this.client.invoke(
+            requests.login({lgname: botAccount, lgpassword: botPassword, lgtoken: lgtoken}),
+        );
+
+        // Step 3. Get CSRF token
+        const {query: {tokens: {csrftoken: csrftoken}}} = await this.client.invoke(
+            requests.query({meta: "tokens"}),
+        );
+        this.csrftoken = csrftoken;
+    }
+}
+
+export class Page {
+
+    private site: Site;
+    public readonly title: string;
+    public text: string;
+    public readonly revisionId: number;
+
+    constructor(site: Site, title: string, text: string, revisionId: number) {
+        this.site = site;
+        this.title = title;
+        this.text = text;
+        this.revisionId = revisionId;
+    }
+
+    /**
+     * Save the page
+     * @param summary Summary of this edit. This message will show on the `history` page.
+     * @param minor Mark this edit as a minor edit
+     * @param botFlag Mark this edit as a bot edit
+     */
+    public async save(summary?: string, minor?: boolean, botFlag?: boolean) {
+        await this.site.client.invoke(
+            requests.edit({
+                title: this.title,
+                text: this.text,
+                summary: summary,
+                minor: minor,
+                token: this.site.csrftoken,
+            })
+        );
+    }
+
+    /**
+     * Delete a page
+     * @param reason The summary of the deletion
+     */
+    public async delete(reason?: string) {
+        await this.site.client.invoke(
+            requests.delete_({
+                title: this.title,
+                reason: reason,
+                token: this.site.csrftoken,
+            })
+        );
+    }
+}
--- api/mod.ts
+++ api/mod.ts
@@ -1,2 +1,3 @@
 export * from "./client.ts";
 export * from "./requests.ts";
+export * from "./bot.ts";
